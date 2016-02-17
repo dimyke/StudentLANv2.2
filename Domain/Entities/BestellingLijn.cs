553e5acd42c25afdea22b3d0109078ea21942ea2
dimyke <dimitrivantillo@gmail.com>
2016-02-17 23:32:11 +0100

    .

---
+++ Domain/Entities/BestellingLijn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class BestellingLijn
+    {
+        public int BestellingLijnId { get; set; }
+
+        public int Aantal { get; set; }
+
+        public int ConsumptieId { get; set; }
+
+
+    }
+}
