fbdd931c4d4efbaa10c5d779bbb4ca2b1c7cafd4
dimyke <dimitrivantillo@gmail.com>
2016-02-17 23:32:38 +0100

    .

---
+++ Domain/Entities/Gebruiker.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Gebruiker : IdentityUser
+    {
+        public override string Id { get; set; }
+        public override string UserName { get; set; }
+        public override string Email { get; set; }
+
+        public string Avatar { get; set; }
+        public string Naam { get; set; }
+        public string Voornaam { get; set; }
+        public string Postcode { get; set; }
+        public DateTime GeboorteDatum { get; set; }
+        public string Origin { get; set; }
+        public string Steam { get; set; }
+        public string Nickname { get; set; }
+        public string BatlleNet { get; set; }
+        public string Wargaming { get; set; }
+
+        public bool NewsletterSubscription { get; set; }
+
+        // foreign key
+        // public int WalletId { get; set; }
+        public int TeamId { get; set; }
+    }
+}
