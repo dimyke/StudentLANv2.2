38e2e6abff1e23cc397fda3b3a96c76eaa5b4c35
dimyke <dimitrivantillo@gmail.com>
2016-02-17 23:29:54 +0100

    aanmaken entitites

---
+++ Domain/Entities/Bestelling.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class Bestelling
+    {
+        public int BestellingId { get; set; }
+        public int GebruikerId { get; set; }
+    }
+}
