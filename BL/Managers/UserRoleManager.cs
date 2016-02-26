6beeec4955b726ed2153532b9a82dd11d482a2e2
dimyke <dimitrivantillo@gmail.com>
2016-02-26 17:14:02 +0100

    added controllers + change in user entity

---
+++ BL/Managers/UserRoleManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+namespace BL.Managers
+{
+    class UserRoleManager : RoleManager<IdentityRole>
+    {
+        private UserRoleManager(IRoleStore<IdentityRole, string> store) : base(store)
+        {
+
+        }
+
+        public static UserRoleManager Create(IdentityFactoryOptions<UserRoleManager> options, IOwinContext context)
+        {
+            return new UserRoleManager(new RoleStore<IdentityRole>(context.Get<StulanContext>()));
+        }
+    }
+    
+    
+}
