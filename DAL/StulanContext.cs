56439e2d82b2f92c4eeb586175b4015cd96db7ca
kurris1@hotmail.com <kurris1@hotmail.com>
2016-02-26 15:53:51 +0100

    dal

--- DAL/StulanContext.cs
+++ DAL/StulanContext.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Domain.Entities;
 
 namespace DAL
 {
-    public class Class1
+    public class StulanContext : DbContext
     {
     }
 }
