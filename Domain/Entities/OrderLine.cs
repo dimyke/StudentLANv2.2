c8ef79fdf659d99146c37e6dba1652e52ceb4b84
dimyke <dimitrivantillo@gmail.com>
2016-02-26 15:54:47 +0100

    rotzooi

---
+++ Domain/Entities/OrderLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class OrderLine
+    {
+        public int OrderLineId { get; set; }
+
+        public int Number { get; set; }
+
+        public int ConsumptionId { get; set; }
+
+        public int OrderId { get; set; }
+
+
+    }
+}
