06a39072dc4e6bfedb64c5ffc500c00024c32d34
kurris1@hotmail.com <kurris1@hotmail.com>
2016-03-01 16:11:37 +0100

    interface consumption

--- BL/Managers/ConsumptionManager.cs
+++ BL/Managers/ConsumptionManager.cs
@@ -2,6 +2,8 @@
 using DAL.Repositories.Contracts;
 using DAL.Repositories.EntitiyFramework;
 using Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BL.Managers
 {
@@ -12,6 +14,11 @@ namespace BL.Managers
         public Consumption Find(int id)
         {
             return _ConsumptionRepository.Find(id);
+        }
+
+        public IEnumerable<Consumption> All()
+        {
+            return _ConsumptionRepository.All();
         }
     }
 }
