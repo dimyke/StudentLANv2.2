fdecc6dafc997db1b8eda483d36419b6426a3b9d
kurris1@hotmail.com <kurris1@hotmail.com>
2016-03-04 16:34:16 +0100

    userrepo

---
+++ DAL/Repositories/Contracts/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+
+namespace DAL.Repositories.Contracts
+{
+    public interface IUserRepository
+    {
+        ApplicationUser FindUser(string id);
+        IEnumerable<ApplicationUser> GetAllApplicationUsers();
+        IEnumerable<ApplicationUser> GetAllApplicationUsersRole(int roleId);
+        void UpdateUser(string id, ApplicationUser user);
+        void DeleteUser(string id);
+        void ChargeWallet(string id, double amount);
+
+    }
+}
