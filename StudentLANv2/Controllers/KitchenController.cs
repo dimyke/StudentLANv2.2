9226e6fb4f41d1f0fe8b8515505c25ddb1058b5b
kurris1@hotmail.com <kurris1@hotmail.com>
2016-03-05 17:48:09 +0100

    kitchen

---
+++ StudentLANv2/Controllers/KitchenController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BL.Managers;
+
+namespace StudentLANv2.Controllers
+{
+    public class KitchenController : Controller
+    {
+
+        private readonly OrderManager _orderManager = new OrderManager();
+        // GET: Kitchen
+         public ActionResult Index()
+        {
+            return View(_orderManager.AllUnfinishedKitchenOrders().ToList());
+        }
+    }
+}
