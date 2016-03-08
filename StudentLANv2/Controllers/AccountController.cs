d761f5bb4f914accbe7f221b9acad927110bf4cd
dimyke <dimitrivantillo@gmail.com>
2016-03-08 19:04:25 +0100

    account edit

--- StudentLANv2/Controllers/AccountController.cs
+++ StudentLANv2/Controllers/AccountController.cs
@@ -498,6 +498,7 @@ namespace StudentLANv2.Controllers
         // POST: KitchenOrders/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // TODO: ervoor zorgen dat wallet niet aangeraakt kan worden!
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult EditAccount(ApplicationUser user)
@@ -505,7 +506,7 @@ namespace StudentLANv2.Controllers
             if (ModelState.IsValid)
             {
                 UserManager.Update(User.Identity.GetUserId(), user);
-                return RedirectToAction("Index");
+                return View(user);
             }
             return View(user);
         }
