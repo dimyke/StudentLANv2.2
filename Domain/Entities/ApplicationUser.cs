f3de4f38455b0d87106812cb049375e96e8c394d
kurris1@hotmail.com <kurris1@hotmail.com>
2016-03-09 23:11:22 +0100

    inloggen met username

--- Domain/Entities/ApplicationUser.cs
+++ Domain/Entities/ApplicationUser.cs
@@ -21,6 +21,7 @@ namespace Domain.Entities
         public DateTime? DateOfBirth { get; set; }
         public string Origin { get; set; }
         public string Steam { get; set; }
+
         public string Nickname { get; set; }
         public string BatlleNet { get; set; }
         public string Wargaming { get; set; }
