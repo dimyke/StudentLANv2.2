a873bab34e70173366fbaa7260a8e98816118b76
dimyke <dimitrivantillo@gmail.com>
2016-03-10 19:48:07 +0100

    something changed

--- DAL/StulanInitializer.cs
+++ DAL/StulanInitializer.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace DAL
 {
-    public class StulanInitializer : DropCreateDatabaseIfModelChanges<StulanContext>
+    public class StulanInitializer : CreateDatabaseIfNotExists<StulanContext>
     {
         protected override void Seed(StulanContext context)
         {
