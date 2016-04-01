6bc30a9ccd78117b1324a3a0352543a200c1257e
dimyke <dimitrivantillo@gmail.com>
2016-04-01 22:48:58 +0200

    added "are you sure you want to delete x" popup for toggle delete

--- StudentLANv2/App_Start/BundleConfig.cs
+++ StudentLANv2/App_Start/BundleConfig.cs
@@ -21,6 +21,7 @@ namespace StudentLANv2
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/StuLanJS.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
---
+++ StudentLANv2/Scripts/StuLanJS.js
@@ -0,0 +1,14 @@
+﻿$('#popUp_AreUSure').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var id = button.data('orderid')
+    var name = button.data('ordername')    
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    console.log(modal.find('.orderid-hier'));
+    modal.find('.orderid-hier').attr('onclick', 'location.href=\'/KitchenAdmin/ToggleDelete?orderid=' + id + '\'')
+    modal.find('.modal-body').text('Ben je zeker dat je orderid: ' + id + ' van user: ' + name + ' wilt verwijderen?')
+
+
+})
+//.onclick('location.href = \'@Url.Action("ToggleDelete", "KitchenAdmin", new { orderid =' + id + '})\'')
