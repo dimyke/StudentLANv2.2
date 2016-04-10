c62507f4896bf1a3f2824bb3cbb43af8270420d5
dimyke <dimitrivantillo@gmail.com>
2016-04-10 14:43:29 +0200

    only authorized users can do something with kitchenorders

--- StudentLANv2/Controllers/KitchenOrdersController.cs
+++ StudentLANv2/Controllers/KitchenOrdersController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNet.Identity;
 
 namespace StudentLANv2.Controllers
 {
+    [Authorize]
     public class KitchenOrdersController : Controller
     {
         private readonly OrderManager _orderManager = new OrderManager();
